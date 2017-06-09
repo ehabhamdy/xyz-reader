1032d3a5977495690a8184ee4a302495e8a6406a
ehabhamdy <ehabhamdy2012@gmail.com>
2017-06-09 12:16:07 +0200

    Fix main screen image aspect ratio

--- XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
@@ -27,8 +27,8 @@ public class DynamicHeightNetworkImageView extends NetworkImageView {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int measuredWidth = getMeasuredWidth();
         setMeasuredDimension(measuredWidth, (int) (measuredWidth / mAspectRatio));
+        super.onMeasure(widthMeasureSpec,  (int) (measuredWidth / mAspectRatio));
     }
 }
